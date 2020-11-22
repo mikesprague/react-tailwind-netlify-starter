7c54fcb7f234d165c64e0bbb2635e73e74760bf0
Michael Sprague <mikesprague@gmail.com>
2020-11-22 11:30:00 -0500

    :recycle: Add proptypes import

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import './App.scss';
 
 export default function App() {
   return (
     <div className="container page-wrapper">
-      <h1>My Netlify Starter Template</h1>
+      <h1>My Netlify React/Tailwind CSS Starter Template</h1>
     </div>
   );
 }
+
+App.propTypes = {
+  // define proptypes here
+};
