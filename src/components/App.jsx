9fca2cdab432aa0eb7ad244aa0fc7d228d7b1286
Michael Sprague <mikesprague@gmail.com>
2023-09-19 18:17:52 -0400

    refactor: misc improvements

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,54 +1,61 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { apiUrl, initIcons } from '../modules/helpers';
+import { apiUrl } from '../modules/helpers';
+import { FaGithub, FaQuoteLeft } from 'react-icons/fa';
+
 import './App.scss';
 
 export default function App() {
-  initIcons();
-
   const [data, setData] = useState(null);
+
   useEffect(() => {
     const getRemoteData = async () => {
       const remoteData = await axios
         .get(`${apiUrl()}/example-function`)
         .then((response) => response.data);
+
       setData(remoteData);
     };
+
     getRemoteData();
   }, []);
 
   return (
     <>
-      <div className="container max-w-screen-xl min-h-screen p-8 text-center page-wrapper">
-        <h1 className="mb-8 text-4xl font-bold text-center underline">
-          My React/Tailwind CSS Netlify Starter Template
-          <br />
-        </h1>
-        <h2 className="mb-12 text-2xl font-semibold leading-tight text-center">
-          Example data below fetched from Netlify function
-          <br />
-          <small className="text-base font-normal text-center">
-            <a href="https://icanhazdadjoke.com/api" rel="noopener noreferrer" target="_blank">
-              (datasource: https://icanhazdadjoke.com/api)
-            </a>
-          </small>
-        </h2>
-        <blockquote className="mx-auto text-3xl italic leading-normal text-center text-blue-200">
-          {data ? (
-            <>
-              <FontAwesomeIcon icon="quote-left" fixedWidth />
-              {data.joke}
-            </>
-          ) : (
-            '... loading dad joke ...'
-          )}
-        </blockquote>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content text-center">
+          <div className="">
+            <h1 className="text-5xl font-bold">
+              My React/Tailwind CSS Netlify Starter Template
+            </h1>
+            <blockquote className="mx-auto text-3xl italic leading-normal text-center text-blue-200">
+              {data ? (
+                <>
+                  <FaQuoteLeft />
+                  {` ${data.joke}`}
+                </>
+              ) : (
+                '... loading dad joke ...'
+              )}
+            </blockquote>
+            <h2 className="my-12 text-2xl font-semibold leading-tight text-center">
+              <small className="text-base font-normal text-center">
+                <a
+                  href="https://icanhazdadjoke.com/api"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  source: https://icanhazdadjoke.com/api
+                </a>
+              </small>
+            </h2>
+          </div>
+        </div>
       </div>
       <div className="fixed min-w-full text-base text-center bottom-2">
         <a href="https://github.com/mikesprague/react-tailwindcss-netlify-starter">
-          <FontAwesomeIcon icon={['fab', 'github']} fixedWidth />
-          Back to repo
+          <FaGithub />
+          {' Back to repo'}
         </a>
       </div>
     </>
--- src/modules/helpers.js
+++ src/modules/helpers.js
@@ -1,13 +1,3 @@
-import { faQuoteLeft, faQuoteRight } from '@fortawesome/free-solid-svg-icons';
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
-import { library } from '@fortawesome/fontawesome-svg-core';
-
-export function initIcons() {
-  library.add(
-    faGithub, faQuoteRight, faQuoteLeft,
-  );
-}
-
 export function isDev() {
   if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
     return true;
